65c2d347e4f0bba9329fd7e31fe6cb7eb9c68af0
cogitoboy <dale.applegate@gmail.com>
2018-12-07 17:04:10 -0700

    removed unused import

--- src/main/java/org/dale/applegate/WeatherserviceApplication.java
+++ src/main/java/org/dale/applegate/WeatherserviceApplication.java
@@ -2,7 +2,7 @@ package org.dale.applegate;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
