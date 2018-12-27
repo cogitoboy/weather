d337f91a8e5410fa2c468ee23cf9a8fab7c5124f
Dale Applegate <daleapplegate@gmail.com>
2018-12-26 19:47:10 -0700

    Added style

--- src/main/java/org/stalesoft/web/controller/WelcomeController.java
+++ src/main/java/org/stalesoft/web/controller/WelcomeController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WelcomeController {
 
 	
+	@GetMapping("/")
+    public String home(Model model) {
+       
+        return "index";
+    }
+	
 	@GetMapping("/welcome")
     public String greeting(Model model) {
        
