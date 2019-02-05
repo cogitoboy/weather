f16f67beda0ac2da45fabbe861a3d8aa30467873
user <user@users-MacBook-Air.local>
2019-02-04 19:51:30 -0700

    issue_5 added blank home page

---
+++ src/main/java/org/stalesoft/web/controller/DashboardController.java
@@ -0,0 +1,17 @@
+package org.stalesoft.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+	
+
+	@GetMapping("/app/home")
+    public String home(Model model) {
+       
+        return "app/home";
+    }
+}
