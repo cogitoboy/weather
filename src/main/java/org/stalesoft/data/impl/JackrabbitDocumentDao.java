707801ce58f6c21e3dc065461a5813aa94fa0048
Dale Applegate <dapplegate@mpqhf.org>
2019-02-20 08:51:22 -0700

    morning commit

--- src/main/java/org/stalesoft/data/impl/JackrabbitDocumentDao.java
+++ src/main/java/org/stalesoft/data/impl/JackrabbitDocumentDao.java
@@ -4,14 +4,18 @@ import javax.jcr.Session;
 import javax.jcr.SimpleCredentials;
 
 import org.apache.jackrabbit.core.RepositoryImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.stalesoft.data.DocumentDao;
 import org.stalesoft.model.Document;
+import org.stalesoft.web.controller.LoggingAccessDeniedHandler;
 
 @Repository("documentDao")
 public class JackrabbitDocumentDao implements DocumentDao {
-
+	
+	private static Logger log = LoggerFactory.getLogger(JackrabbitDocumentDao.class);
 
 	@Override
 	public void saveDocument(Document document) {
