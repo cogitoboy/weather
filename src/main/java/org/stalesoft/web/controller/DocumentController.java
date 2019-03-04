511d8f634f992d5ce1e262d5347066d620cf035c
Dale Applegate <dapplegate@mpqhf.org>
2019-03-04 10:56:00 -0700

    clean up

--- src/main/java/org/stalesoft/web/controller/DocumentController.java
+++ src/main/java/org/stalesoft/web/controller/DocumentController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
-import org.apache.tomcat.jni.File;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,17 +12,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.stalesoft.model.Document;
-import org.stalesoft.model.Folder;
 import org.stalesoft.service.DocumentService;
-import org.stalesoft.web.dto.DocumentDto;
 import org.stalesoft.web.dto.DocumentListDto;
 import org.stalesoft.web.dto.SearchDto;
+
+//TODO: Add console logging.
 
 @Controller
 public class DocumentController {
@@ -53,7 +49,6 @@ public class DocumentController {
 	 * @return
 	 */
 	
-	//RequestMapping(value = "/app/document", method = RequestMethod.POST)
 	@PostMapping("/app/document")
 	public String uploadDocument(@RequestParam("file") MultipartFile uploadDocument, Model model) {
 		//TODO Log all incoming parameters
