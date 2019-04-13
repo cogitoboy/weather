91a35df385279f79e5fabbf554986ae44d118281
dale <dale.applegate@gmail.com>
2019-04-13 15:53:51 -0600

    prepwork for signup contoller

--- src/main/java/com/stalesoft/welcome/web/controller/SignupController.java
+++ src/main/java/com/stalesoft/welcome/web/controller/SignupController.java
@@ -1,11 +1,33 @@
 package com.stalesoft.welcome.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.stalesoft.welcome.web.dto.SignupDto;
 
 @Controller
 public class SignupController {
 
-	public String here() {
-		return "here";
+	private static Logger log = LoggerFactory.getLogger(SignupController.class);
+	
+	public String createAccount(SignupDto signupDto, Model model) {
+		
+		//validation
+		
+		//create account
+		
+		//respond
+		
+		log.debug("Emails is {}", signupDto.getEmail());
+		
+		return "continue";
+	}
+	
+	@ModelAttribute(value="signup")
+	public SignupDto getSignupDto() {
+		return new SignupDto();
 	}
 }
---
+++ src/main/java/com/stalesoft/welcome/web/dto/SignupDto.java
@@ -0,0 +1,15 @@
+package com.stalesoft.welcome.web.dto;
+
+public class SignupDto {
+	
+	private String email;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
