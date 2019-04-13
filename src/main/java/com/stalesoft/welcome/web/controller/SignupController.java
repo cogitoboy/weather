5f5885dacc5a43cd6c7dc53a4deeb2759fdc1874
dale <dale.applegate@gmail.com>
2019-04-13 14:04:09 -0600

    Added example action state in the webflow

---
+++ src/main/java/com/stalesoft/welcome/web/controller/SignupController.java
@@ -0,0 +1,11 @@
+package com.stalesoft.welcome.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SignupController {
+
+	public String here() {
+		return "here";
+	}
+}
